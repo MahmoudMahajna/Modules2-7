270cf20bd89b736da66c362b8c1462d1e9313026
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-18 12:00:05 +0300

    Lab 2.3

--- GuessingGame/GuessingGame/GuessingGameClass.cs
+++ GuessingGame/GuessingGame/GuessingGameClass.cs
@@ -10,13 +10,25 @@ namespace GuessingGame
             bool flag;
             var times = 1;
             var secret = new Random().Next(1, 101);
+
+
+            //You could have used do-while loop to eliminate this GetInput
             GetInput(out flag, out guessingNumber);
+
+            //Flag is redundant.
             while (guessingNumber != secret || !flag)
             {
+                //flag is redundant
+                //What is this calculation for? Why not guessingNumber > secret?
+                //10 is too large for an epsilon...So large, that it make the calculation wrong.
                 if (flag && guessingNumber - secret > 10)
                     Console.WriteLine("too big!");
+
+                //flag is redundant
+                //same as above.
                 if (flag && guessingNumber - secret < -10)
                     Console.WriteLine("too small");
+
                 GetInput(out flag, out guessingNumber);
                 if (++times != 7) continue;
                 Console.WriteLine($"you failed, the right number is {secret}");
